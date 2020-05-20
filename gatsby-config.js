1fd9910dde611c85013c025226beb95c19d0cadb
Vittawat <vittawat@Vittawats-MacBook-Pro-3.local>
2020-05-20 16:48:33 +0700

    First commit plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,24 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+
+ 
   plugins: [
+
+
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -27,8 +38,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    `gatsby-plugin-sass`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
 }
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+import Header2 from '../components/header2';
+import "../components/style.css"
+import Data from "../data/data.yml"
+
+import Footer from "../components/footer"
+import "../styles/styles.scss"
+
+
+
+
+export default (props) => {
+  console.log(props.data)
+  return (
+
+
+    <div className="indexcontainer">
+
+      <Header2 />
+
+      <div className="indextext">
+        {Data.indextext}
+      </div>
+      <Footer />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
 
-export default IndexPage
+
+
+  )
+}
+
+
+export const query = graphql`
+  query {
+   background: file(relativePath: {eq: "background2.png"}){
+      childImageSharp {
+        fluid(maxWidth:1440, quality:100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
