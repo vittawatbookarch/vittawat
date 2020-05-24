02c9c410fe04da77a1b311123cfddc0c740c1b71
Vittawat <vittawat@Vittawats-MacBook-Pro-3.local>
2020-05-25 03:51:35 +0700

    Projects name changed

--- src/pages/design.js
+++ src/pages/design.js
@@ -30,15 +30,15 @@ export default (props) => {
         <Link to="wave" className="two">  <Img fluid={props.data.b.childImageSharp.fluid} /></Link> <p className="undertext2">Wave installation</p>
         <Link to="regen" className="three">  <Img fluid={props.data.chair.childImageSharp.fluid} /> </Link> <p className="undertext3">Regen chair</p>
         <Link to="hana" className="four">  <Img fluid={props.data.hana.childImageSharp.fluid} /></Link>  <p className="undertext">Hana </p>
-        <Link to="treelamp" className="five">  <Img fluid={props.data.treelamp.childImageSharp.fluid} /></Link> <p className="undertext2">Fabricate installation </p>
-        <Link to="fabricate" className="six">  <Img fluid={props.data.fabricate.childImageSharp.fluid} /> </Link> <p className="undertext3"> Kids treehouse </p>
+        <Link to="treelamp" className="five">  <Img fluid={props.data.treelamp.childImageSharp.fluid} /></Link> <p className="undertext2">Outdoor treelamp </p>
+        <Link to="fabricate" className="six">  <Img fluid={props.data.fabricate.childImageSharp.fluid} /> </Link> <p className="undertext3"> Fabricate </p>
         <Link to="dp" className="seven">  <Img fluid={props.data.dp.childImageSharp.fluid} /> </Link><p className="undertext">Curve gem</p>
         <Link to="boodwood" className="eight">  <Img fluid={props.data.boodwood.childImageSharp.fluid} /> </Link> <p className="undertext2">Boodwood</p>
         <Link to="int" className="nine">  <Img fluid={props.data.room.childImageSharp.fluid} /> </Link><p className="undertext3">Renovated casual living room</p>
         <Link to="diamondpearl" className="ten">  <Img fluid={props.data.diamondpearl.childImageSharp.fluid} /> </Link>
         <p className="undertext">Diamondpearl installation</p>
         <Link to="kids" className="eleven">  <Img fluid={props.data.treehouse.childImageSharp.fluid} /> </Link>
-        <p className="undertext2">Outdoor treelamp</p>
+        <p className="undertext2">Kids treehouse</p>
 
       </div >
 
